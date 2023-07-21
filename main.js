b6e1cc02494487ca67153bb6d22f06bcdc2570a9
Midoriya0312 <oybektojiboyev0312@gmail.com>
2023-07-21 10:12:06 +0500

    lesson15

---
+++ main.js
@@ -0,0 +1,70 @@
+// const person = {
+//     name: 'Sara',
+//     age: 25,
+//     gender: 'femele'
+// }
+
+// let {name: name1, age: age1, gender: gender1} = person;
+
+// console.log(name1);
+// console.log(age1);
+// console.log(gender1);
+
+
+// Homework arrow fnc
+
+// 1
+
+// let myFunction = (a, b) => a * b;
+
+// 2
+
+// hello = function() {
+//     return "Hello World!";
+//   }
+
+// 3
+
+// hello = () => {
+//     return "Hello World!";
+//   }
+
+
+// 4
+
+// hello = () => "Hello World!";
+
+// 5
+
+// hello = (val) => "Hello " + val;
+
+// 6
+
+// hello = val => "Hello " + val;
+
+// swapping
+
+// 1
+
+// let a;
+// let b;
+
+// [a, b] = [1, 2, 3];
+
+// console.log(a);
+// console.log(b);
+
+
+// 2
+
+// let a = 1;
+// let b = 2;
+
+// [a, b] = [b, a];
+
+// console.log(a);
+// console.log(b);
+
+// 3
+
+
